159a75900a359da438a828e11efbefc8f82c28b6
Yuri <50510523+yuradun@users.noreply.github.com>
2020-02-22 13:06:41 +0100

    Отшлифовка и конечные правки

--- js/main.js
+++ js/main.js
@@ -1,41 +1,43 @@
 'use strict';
 
 var messages = ['В целом всё неплохо. Но не всё.',
- 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
- 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
- 'Всё отлично!',
- 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
- 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.'];
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Всё отлично!',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.'];
 
 var names = ['Илья', 'Юра', 'Настя', 'Катя', 'Илон', 'Стив'];
 
-var createOneRandomElement = function (mass) { //ФУНКЦИЯ ДЛЯ ОПРЕДЕЛЕНИЯ ОДНОГО РАНДОМНОГО ЭЛЕМЕНТА МАССИВА
-  return mass[randomInteger (0, mass.length)];
-}
+var descriptions = ['Все клево, отдыхаю', 'Попиваю сок у себя на квартале', 'Отдыхаю после рабочего дня', 'Сегодня пятница!', 'Ставьте лайки!1!'];
 
-var createTwoRandomElement = function (mass) { //ФУНКЦИЯ ДЛЯ ОПРЕДЕЛЕНИЯ ДВУХ РАНДОМНЫХ ЭЛЕМЕНТОВ МАССИВА
-  var firstElement = mass[randomInteger (0, mass.length)];
-  var secondElement = mass[randomInteger (0, mass.length)];
-  if (firstElement == secondElement)
+var createOneRandomElement = function (mass) { //  ФУНКЦИЯ ДЛЯ ОПРЕДЕЛЕНИЯ ОДНОГО РАНДОМНОГО ЭЛЕМЕНТА МАССИВА
+  return mass[randomInteger(0, mass.length)]
+};
+
+var createTwoRandomElement = function (mass) { // ФУНКЦИЯ ДЛЯ ОПРЕДЕЛЕНИЯ ДВУХ РАНДОМНЫХ ЭЛЕМЕНТОВ МАССИВА
+  var firstElement = mass[randomInteger(0, mass.length)];
+  var secondElement = mass[randomInteger(0, mass.length)];
+  if (firstElement === secondElement)
     secondElement += 1;
   return firstElement + ' ' + secondElement;
-}
+};
 
-var photos = []; //МАССИВ ДЛЯ ОБЪЕКТОВ
+var photos = []; // МАССИВ ДЛЯ ОБЪЕКТОВ
 
-var createPhotos = function () {
+var addPhotos = function () {
   for (var i = 1; i < 26; i++) {
-    var randomNumberForMess = randomInteger (0, 1); //ГЕНЕРИРУЕМ СЛУЧАЙНОЕ ЧИСЛО ДЛЯ message(randomNumberForMess)
+    var randomNumberForMess = randomInteger(0, 1); // ГЕНЕРИРУЕМ СЛУЧАЙНОЕ ЧИСЛО ДЛЯ message(randomNumberForMess)
     var photosDescription = {
-      url : 'photos/' + i + '.jpg',
-      description : 'Адихаю',
-      likes : randomInteger (15, 200),
-      comments : {
-        avatar : "img/avatar-" + randomInteger (1, 6) + ".svg",
-        message : randomNumberForMess == 0 ? createOneRandomElement (messages) : createTwoRandomElement (messages),
-      	name : createOneRandomElement (names)
+      url: 'photos/' + i + '.jpg',
+      description: createOneRandomElement(descriptions),
+      likes: randomInteger(15, 200),
+      comments: {
+        avatar: 'img/avatar-' + randomInteger(1, 6) + '.svg',
+        message: randomNumberForMess === 0 ? createOneRandomElement(messages) : createTwoRandomElement(messages),
+      	name: createOneRandomElement(names)
       }
-    }
-  	photos.push (photosDescription); //ДОБАВЛЕНИЕ КАЖДОГО СГЕНЕРИРОВАННОГО ОБЪЕКТА В МАССИВ PHOTOS
+    };
+    photos.push(photosDescription); // ДОБАВЛЕНИЕ КАЖДОГО СГЕНЕРИРОВАННОГО ОБЪЕКТА В МАССИВ PHOTOS
   }
-}
+};
